9c0fef5d102c87664a417a1451b042f77a796396
Marlon Junior <marlonrfjunior@outlook.com>
2022-09-21 18:10:24 -0300

    Resolve results

--- SeeFood/ViewController.swift
+++ SeeFood/ViewController.swift
@@ -32,9 +32,35 @@ extension ViewController : UIImagePickerControllerDelegate {
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage{
             imageView.image = userPickedImage
-            let corImage = CIImage(image: userPickedImage)
-            
+            guard let corImage = CIImage(image: userPickedImage) else {
+                fatalError("Could not convert UIImage into CIImage")
+            }
+            detect(image: corImage)
         }
         imagePicker.dismiss(animated: true)
+       
+    }
+}
+// MARK: - CoreML functions
+extension ViewController {
+    func detect(image : CIImage){
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else{
+            fatalError("Loading CoreML Model Failed.")
+        }
+        let request = VNCoreMLRequest(model: model){(request , error) in
+            guard let result = request.results as? [VNClassificationObservation] else {
+                fatalError("Model failed to process image")
+            }
+            if let firstResult = result.first {
+                self.navigationItem.title = firstResult.identifier
+            }
+        }
+    let handler = VNImageRequestHandler(ciImage: image)
+        do{
+            try handler.perform([request])
+        }catch{
+            print(error)
+        }
+        
     }
 }
